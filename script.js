aa73cc97d3f6208cc311dd6492d0d1b5ccc36604
zamsyntax <136183289+zamsyntax@users.noreply.github.com>
2024-08-13 08:28:18 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+// Fungsi untuk membuka link di tab baru
+function openLink(url) {
+    window.open(url, '_blank');
+}
+
+// Mendapatkan elemen audio
+const backgroundMusic = document.getElementById('background-music');
+
+// Menambahkan event listener untuk tombol mute/unmute (jika ada)
+// ... (kode untuk tombol mute/unmute) ... 
